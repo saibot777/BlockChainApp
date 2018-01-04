6c3005dcd27e171732d85d20421697426cce59e7
saibot777 <stefantrajkovic777@gmail.com>
2018-01-04 11:16:04 +0100

    first phase over, testing...

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const SHA256 = require('crypto-js/hmac-sha256');
+const SHA256 = require('crypto-js/sha256');
 
 class Block {
 
-    constructor(index, timestamp, data, previousHash = '') {
+    constructor(index, timestamp, data, previousHash = '', hash) {
         this.index = index;
         this.timestamp = timestamp;
         this.data = data;
@@ -21,7 +21,7 @@ class BlockChain {
     }
 
     createGenesisBlock() {
-        return new Block(0, "01/01/2017", "Genesis block", "0");
+        return new Block(0, "01/01/2018", "Genesis block", "0");
     }
 
     getLatestBlock() {
@@ -30,5 +30,13 @@ class BlockChain {
 
     addBlock(newBlock) {
         newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
     }
 }
+
+let frenzieCoin = new BlockChain();
+frenzieCoin.addBlock(new Block(1, "02/01/2018", { amount: 4 }));
+frenzieCoin.addBlock(new Block(2, "04/01/2018", { amount: 10 }));
+
+console.log(JSON.stringify(frenzieCoin, null, 4));
